33ab11d81f6d26adee82b11bcc7996f5a1c3b451
ahmadkaleem123 <muhammad.ahmad.kaleem@gmail.com>
2018-09-26 09:45:48 +0500

    Add files via upload

---
+++ morse_receive.ino
@@ -0,0 +1,153 @@
+int one [5] = {1,3,3,3,3};
+int two [5] = {1,1,3,3,3};
+int three [5] = {1,1,1,3,3};
+int four [5] = {1,1,1,1,3};
+int five [5] = {1,1,1,1,1};
+int six [5] = {3,1,1,1,1};
+int seven [5] = {3,3,1,1,1};
+int eight [5] = {3,3,3,1,1};
+int nine [5] = {3,3,3,3,1};
+int zero [5] = {3,3,3,3,3};
+int num [5];
+int ldr = A0;
+int first = 0;
+int value;
+int i = 0;
+int count;
+int a = 1;
+int calib = 0;
+int b;
+void setup() {
+  Serial.begin(9600);
+  pinMode(ldr, INPUT);
+  Serial.println("Please send a number through the laser.");
+}
+
+void loop() {
+  if(calib==0){
+     b = analogRead(ldr);
+     calib++;
+  }
+  value = analogRead(ldr);
+  /*Serial.println(value);*/
+  if(i<5){
+    if (value>b+100){
+      while(value>b+100){
+        value = analogRead(ldr);
+        count++;
+        delay(100);
+      }
+      count = count *100;
+      count = count / 1000;
+      num[i] = count; 
+      count = 0;
+      i++;
+    }
+  }
+  else if(i==5){
+    i = 0;
+    for(int j=0;j<5;j++){
+      if(num[j] != one[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 1");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != two[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 2");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != three[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 3");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != four[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 4");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != five[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 5");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != six[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 6");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != seven[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 7");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != eight[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 8");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != nine[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 9");
+    }
+    a=1;
+    for(int j=0;j<5;j++){
+      if(num[j] != zero[j]){
+        a = 0;
+        break;
+      }
+    }
+    if(a==1){
+       Serial.println("Received: 0");
+    }
+    a=1;
+    Serial.println("Please send a number through the laser.");
+  }
+  delay(100);
+  count=0;
+}
---
+++ morse_send.ino
@@ -0,0 +1,112 @@
+int one [5] = {1,3,3,3,3};
+int two [5] = {1,1,3,3,3};
+int three [5] = {1,1,1,3,3};
+int four [5] = {1,1,1,1,3};
+int five [5] = {1,1,1,1,1};
+int six [5] = {3,1,1,1,1};
+int seven [5] = {3,3,1,1,1};
+int eight [5] = {3,3,3,1,1};
+int nine [5] = {3,3,3,3,1};
+int zero [5] = {3,3,3,3,3};
+int num [5];
+int laser = 13;
+char state;
+int first = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(laser, OUTPUT);
+  Serial.println("Please give a number between 0 and 9 to send as morse code.");
+}
+
+void loop() {
+  
+  if(Serial.available()>0)
+  {
+    state = Serial.read();
+    switch(state) {
+    case '0':
+      for (int j=0;j<5;j++){
+        num[j] = zero[j];
+      }
+      first = 0;
+      break;
+      
+    case '1':
+      for (int j=0;j<5;j++){
+        num[j] = one[j];
+      }
+      first = 0;
+      break;
+      
+    case '2':
+      for (int j=0;j<5;j++){
+        num[j] = two[j];
+      }
+      first = 0;
+      break;
+      
+    case '3':
+      for (int j=0;j<5;j++){
+        num[j] = three[j];
+      }
+      first = 0;
+      break;
+      
+    case '4':
+      for (int j=0;j<5;j++){
+        num[j] = four[j];
+      }
+      first = 0;
+      break;
+      
+    case '5':
+      for (int j=0;j<5;j++){
+        num[j] = five[j];
+      }
+      first = 0;
+      break;
+     
+    case '6':
+      for (int j=0;j<5;j++){
+        num[j] = six[j];
+      }
+      first = 0;
+      break;
+      
+    case '7':
+      for (int j=0;j<5;j++){
+        num[j] = seven[j];
+      }
+      first = 0;
+      break;
+      
+    case '8':
+      for (int j=0;j<5;j++){
+        num[j] = eight[j];
+      }
+      first = 0;
+      break;
+      
+    case '9':
+      for (int j=0;j<5;j++){
+        num[j] = nine[j];
+      }
+      first = 0;
+      break;
+    }
+    if(first == 0){
+      for (int i=0;i<5;i++){
+      digitalWrite(laser,HIGH);
+      delay((num[i]*1000));
+      digitalWrite(laser, LOW);
+      delay(1000);
+      }
+      first = 1;
+    } 
+    
+  }
+  
+  
+  
+
+}
